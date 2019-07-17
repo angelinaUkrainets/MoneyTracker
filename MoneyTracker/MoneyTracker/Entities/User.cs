725a4565d0f7be7c3b01dcf78dad1a650c53ee7e
angelyusha.u <angelyusha.u@gmail.com>
2019-07-17 09:24:57 +0300

    Start Entity framework

---
+++ MoneyTracker/MoneyTracker/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTracker.Entities
+{
+    class User
+    {
+        [Key] public int ID { get; set; }
+
+        [Required] public string Name { get; set; }
+
+        [Required] public string Surname { get; set; }
+
+        [Required, StringLength(50), Index(IsUnique = true)] public string Login { get; set; }
+
+        [Required, EmailAddress] public string Email { get; set; }
+
+        [Required, StringLength(30, MinimumLength = 6), DataType(DataType.Password)] public string Password { get; set; }
+    }
+}
