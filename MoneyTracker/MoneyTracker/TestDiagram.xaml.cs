fa8a566ae2ba2db3499b3b220700c05830c7fe80
77dasha0377 <77dasha0377@gmail.com>
2019-08-29 13:24:48 +0300

    1111

---
+++ MoneyTracker/MoneyTracker/TestDiagram.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MoneyTracker
+{
+    /// <summary>
+    /// Interaction logic for TestDiagram.xaml
+    /// </summary>
+    public partial class TestDiagram : Window
+    {
+        public TestDiagram()
+        {
+            InitializeComponent();
+        }
+       
+    }
+}
